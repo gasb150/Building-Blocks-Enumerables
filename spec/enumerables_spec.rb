81d4ea9913724291e553807fc53cf614c83ce883
gasb150 <gasb_150@hotmail.com>
2020-12-14 15:12:23 -0500

    add my_select test

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -7,9 +7,15 @@ require './enumerables'
      expect(array.my_each { |friend| friend }).to eql(array)
     end
    end
+
    context 'my_each_with_index' do
     it "move into every item of array and match with index" do
-     expect(%w[Sharon Leo Leila Brian Arun].my_each_with_index { |friend, index| puts friend if index.even? }).to eql(%w[Sharon Leila Arun])
+     expect(array.my_each_with_index { |friend, i| i }).to eql(array.each_with_index { |friend, i| i })
     end
  end
+  context 'my_select' do
+    it "select elemets of the array" do
+      expect(array.my_select { |friend| friend != 'Brian' }).to eql(%w[Sharon Leo Leila Arun])
+    end
+  end
 end
