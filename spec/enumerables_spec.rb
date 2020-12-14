68ec45ceb7ebb862bfed9a6e3b0ee0e9715bbf7c
gasb150 <gasb_150@hotmail.com>
2020-12-14 16:28:42 -0500

    add my maps and my inject tests

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -102,6 +102,31 @@ describe Enumerable do
 		it 'returns the count of the arguments with same value given inside the array' do
 			expect([1, 3.14, 77].my_count { |x| (x % 2).zero? }).to eql(0)
 		end
-	end
+  end
+  
+  context 'my_maps' do
+    it 'replace the value that match with other one and return the new array' do
+    my_order = ['medium Big Mac', 'medium fries', 'medium milkshake']
+    expect(my_order.my_map { |item| item.gsub('medium', 'extra large') }).to eql(["extra large Big Mac", "extra large fries", "extra large milkshake"])
+    end
+    it 'multiply each element and return a new value' do
+       expect((0..5).my_map { |i| i * i }).to eql((0..5).map { |i| i * i })
+    end
+    
+  end
 
+  context 'my_inject' do
+    it 'return the sum of each elements inside an array' do
+      expect((1..5).my_inject { |sum, n| sum + n }).to eql(15)
+    end
+    it 'return the multiplocation of each elements inside an array' do 
+      expect((1..5).my_inject(1) { |product, n| product * n }).to eql(120)
+    end
+    it 'return the longest word' do
+      longest = %w[ant bear cat].my_inject do |memo, word|
+        memo.length > word.length ? memo : word
+      end
+      expect(longest).to eql("bear")
+    end
+  end
 end
