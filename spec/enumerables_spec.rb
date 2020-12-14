5d0af4c6dc5de394b8ee297b2f8844d0f9f51d7c
gasb150 <gasb_150@hotmail.com>
2020-12-14 17:33:18 -0500

    fix tabs issues

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -3,48 +3,48 @@ require './enumerables'
 describe Enumerable do
   let(:array) {%w[Sharon Leo Leila Brian Arun]}
 
-	context 'my_each' do
-		it 'move into every item of array' do
+  context 'my_each' do
+    it 'move into every item of array' do
       expect(array.my_each { |friend| friend }).to eql(array)
-		end
-	end
+    end
+  end
 
-	context 'my_each_with_index' do
+  context 'my_each_with_index' do
     it 'move into every item of array and match with index' do
       expect(array.my_each_with_index { |friend, i| i }).to eql(array.each_with_index { |friend, i| i })
     end
-	end
+  end
 
-	context 'my_select' do
+  context 'my_select' do
     it 'select elements of the array' do
       expect(array.my_select { |friend| friend != 'Brian' }).to eql(%w[Sharon Leo Leila Arun])
     end
-	end
+  end
 
-	context 'my_all' do
+  context 'my_all' do
     it 'compare if all elements are TRUE' do
       expect(array.my_all? { |word| word.length >= 3 }).to eql(true)
-		end
+    end
 
-		it 'compare if all elements are FALSE' do
+    it 'compare if all elements are FALSE' do
       expect(array.my_all? { |word| word.length >= 4 }).to eql(false)
-		end
+    end
 
-		it "compare if all elements has the letter 'a'" do
+    it "compare if all elements has the letter 'a'" do
       expect(array.my_all?(/a/)).to eql(false)
     end
 
     it "compare if all elements has a numeric value" do
       expect([1, 2i, 3.14].my_all?(Numeric)).to eql(true)
-		end
+    end
 
-		it "return true if the array is empty" do
-			expect([].my_all?).to eql(true)
+    it "return true if the array is empty" do
+      expect([].my_all?).to eql(true)
     end
   end
 
-	context 'my_any' do
-		it 'compare if any element are TRUE' do
+  context 'my_any' do
+    it 'compare if any element are TRUE' do
       expect(array.my_any? { |word| word.length >= 10 }).to eql(false)
     end
 
@@ -62,18 +62,18 @@ describe Enumerable do
 
     it "return true if the any there's any element inside the array, otherwise returns false" do
       expect([].my_any?).to eql(false)
-		end
-	end
+    end
+  end
 
-	context 'my_none' do
-		it 'compare if none element return TRUE' do
-			expect(array.my_none? { |word| word.length >= 10 }).to eql(true)
-		end
+  context 'my_none' do
+    it 'compare if none element return TRUE' do
+      expect(array.my_none? { |word| word.length >= 10 }).to eql(true)
+    end
 
-		it 'compare if none element return FALSE' do
-			expect(array.my_none? { |word| word.length >= 3 }).to eql(false)
-		end
-		it "compare if none element has the letter 'a'" do
+    it 'compare if none element return FALSE' do
+      expect(array.my_none? { |word| word.length >= 3 }).to eql(false)
+    end
+    it "compare if none element has the letter 'a'" do
       expect(array.my_none?(/a/)).to eql(false)
     end
 
@@ -87,21 +87,21 @@ describe Enumerable do
       expect([nil].my_none?).to eql(true)
       expect([true].my_none?).to eql(false)
     end
-	end
+  end
 
-	context 'my_count' do
+  context 'my_count' do
     it 'returns the count of the arguments given inside the array' do
       expect(array.my_count).to eql(5)
-		end
+    end
 
-		it 'returns the count of the arguments with same value given inside the array' do
+    it 'returns the count of the arguments with same value given inside the array' do
       expect(array.my_count('Leo')).to eql(1)
-		end
+    end
 
-		it 'returns the count of the arguments with same value given inside the array' do
-			expect([1, 3.14, 77].my_count { |x| (x % 2).zero? }).to eql(0)
-		end
-	end
+    it 'returns the count of the arguments with same value given inside the array' do
+      expect([1, 3.14, 77].my_count { |x| (x % 2).zero? }).to eql(0)
+    end
+  end
 
   context 'my_maps' do
     it 'replace the value that match with other one and return the new array' do
